0c1eb83e3a3682f5b06caed1c55d261932be4953
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-03-03 06:38:27 -0500

    Change L0 step to handle removed and edited raw files (#261)

---
+++ pipeline/data_PREFLIGHT/Raw/Raw_edited/README.md
@@ -0,0 +1,5 @@
+# Raw
+
+This folder holds raw data files that have been manually edited
+(because they had datalogger-generated problems that caused errors).
+They are substituted for the originals during processing by `L0.qmd`.
---
+++ pipeline/utils/comparator.R
@@ -0,0 +1,50 @@
+# Build a list of differences between v1-2 raw and Dropbox
+
+dropbox_raw <- c("~/Dropbox/COMPASS_PNNL_Data/COMPASS_PNNL_Rawdata_Archive/",
+                 "~/Dropbox/GCREW_LOGGERNET_DATA/archive_data/",
+                 "~/Dropbox/TEMPEST_PNNL_Data/Loggernet_Rawdata_Archive/")
+patterns <- c("\\.dat$", "^GCREW_MET_GCREW_MET_15min", "\\.dat$")
+drfiles <- c()
+for(i in seq_along(dropbox_raw)) {
+    drfiles <- append(drfiles, list.files(dropbox_raw[i], patterns[i], full.names = TRUE))
+}
+
+pipeline_raw <- "data/Raw/"
+prfiles <- list.files(pipeline_raw, "\\.dat$", full.names = TRUE, recursive = TRUE)
+
+message("There are ", length(drfiles), " Dropbox raw files")
+message("There are ", length(prfiles), " Dropbox raw files")
+
+# Identify files that are in Dropbox but not Raw/
+# These are either new (logged since last data release) or were removed
+removals <- setdiff(basename(drfiles), basename(prfiles))
+message("There are ", length(removals), " Dropbox files but not Raw")
+recents <- grep("_2025", removals)
+message("...of these ", length(recents), " have a '_2025' pattern")
+
+rlf <- "data/Raw/removed_raw_files.txt"
+message("Writing removal list to ", rlf)
+if(file.exists(rlf)) stop("Removal file already exists!!")
+writeLines(removals, rlf)
+
+# Identify identically-named files whose contents differ
+differs <- rep(FALSE, length(drfiles))
+pb <- txtProgressBar(1, length(drfiles))
+for(i in seq_along(drfiles)) {
+    setTxtProgressBar(pb, i)
+    dfile <- drfiles[i]
+    dfile_base <- basename(dfile)
+    if(dfile_base %in% removals) next
+    pfile <- prfiles[grep(dfile_base, prfiles, fixed = TRUE)]
+
+    df_md5 <- digest::digest(file = dfile, algo = "md5")
+    pf_md5 <- digest::digest(file = pfile, algo = "md5")
+    differs[i] <- df_md5 != pf_md5
+}
+
+message("\nThere are ", sum(differs), " Dropbox files whose contents differ from Raw")
+message("Copying those files to Raw_edited")
+diff_files <- basename(drfiles[differs])
+for(f in diff_files) {
+    file.copy(file.path("data/Raw/", f), "data/Raw/Raw_edited/")
+}
