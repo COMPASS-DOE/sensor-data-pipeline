7526ce1eed4a8238b4683f97ae4c86798027ad0d
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-05-21 08:15:17 -0400

    Summary table of flags

--- pipeline/L2-utils.R
+++ pipeline/L2-utils.R
@@ -8,29 +8,35 @@
 # sample median
 # See https://en.wikipedia.org/wiki/Median_absolute_deviation
 # and e.g. https://doi.org/10.1016/j.jesp.2013.03.013 and
-QAQC_mad <- function(x, threshold = 3) {
+QAQC_mad <- function(x, threshold = 3, min_n = 100) {
+    if(length(x) < min_n) return(rep(FALSE, length(x)))
+
     md <- mad(x, na.rm = TRUE)
 
     # return vector of flags
     return(abs(x - median(x, na.rm = TRUE)) > md * threshold)
 }
 
-do_outlier_test <- function(x, QAQC_name, time_grouping, params) {
+do_outlier_test <- function(x, QAQC_name, time_grouping, otherparams) {
 
     # Split data
     message("\t\tTime grouping is ", time_grouping)
     time_groups <- lubridate::round_date(x$TIMESTAMP, time_grouping)
-
-    # Split the data
     x_split <- split(x, time_groups)
     message("\t\tData splits: ", length(x_split))
 
-    # For each group, call the appropriate function
     flag_name <- paste0("F_", gsub("QAQC_", "", QAQC_name))
-    f <- match.fun(QAQC_name)
 
+    # Construct expression (the call to QAQC_mad) to evaluate
+    if(is.na(otherparams)) {
+        expr <- paste(QAQC_name, "(x$Value)")
+    } else {
+        expr <- paste(QAQC_name, "(x$Value,", otherparams, ")")
+    }
+
+    # For each group, call the QAQC function
     results <- lapply(x_split, function(x) {
-        x[[flag_name]] <- f(x$Value)
+        x[[flag_name]] <- eval(parse(text = expr)) # f(x$Value)
         x
     })
 
