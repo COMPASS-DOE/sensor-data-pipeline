18823c1c299c6cb2d11f4f28bf7985924cf057fa
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-05-25 17:10:58 -0400

    Cleanup and refactor

--- pipeline/L2-utils.R
+++ pipeline/L2-utils.R
@@ -9,7 +9,7 @@
 # and e.g. https://doi.org/10.1016/j.jesp.2013.03.013
 # Returns a logical vector of same length as input, with TRUE
 # indicating that the corresponding value is an outlier
-QAQC_mad <- function(x, threshold = 3, min_n = 100) {
+outliers_MAD <- function(x, threshold = 3, min_n = 100) {
     if(length(x) < min_n) return(rep(FALSE, length(x)))
 
     md <- mad(x, na.rm = TRUE)
@@ -18,7 +18,7 @@ QAQC_mad <- function(x, threshold = 3, min_n = 100) {
     return(abs(x - median(x, na.rm = TRUE)) > md * threshold)
 }
 
-do_outlier_test <- function(x, QAQC_name, time_grouping, otherparams) {
+do_outlier_test <- function(x, algorithm, time_grouping, otherparams) {
 
     # Split data
     message("\t\tTime grouping is ", time_grouping)
@@ -26,13 +26,14 @@ do_outlier_test <- function(x, QAQC_name, time_grouping, otherparams) {
     x_split <- split(x, time_groups)
     message("\t\tData splits: ", length(x_split))
 
-    flag_name <- paste0("F_", gsub("QAQC_", "", QAQC_name))
+    function_name <- paste0("outliers_", algorithm)
+    flag_name <- paste0("F_", algorithm)
 
     # Construct expression (the call to QAQC_mad) to evaluate
     if(is.na(otherparams)) {
-        expr <- paste(QAQC_name, "(x$Value)")
+        expr <- paste(function_name, "(x$Value)")
     } else {
-        expr <- paste(QAQC_name, "(x$Value,", otherparams, ")")
+        expr <- paste(function_name, "(x$Value,", otherparams, ")")
     }
 
     # For each group, call the QAQC function
@@ -44,6 +45,36 @@ do_outlier_test <- function(x, QAQC_name, time_grouping, otherparams) {
     # Bind and return; we don't do any filtering here
     return(do.call("rbind", results))
 }
+
+# Aggregate (average) the data for each timestep, computing `n` and `n_drop`
+L2_aggregate <- function(x) {
+    # This would be easier and faster to do in dplyr or data.table but
+    # I'm trying to minimize dependencies; so...
+    x$Value[as.logical(x$F_drop)] <- NA
+
+    # aggregate() chokes on NAs in grouping variables
+    x <- replace_na(x, list(Instrument_ID = "", Sensor_ID = "", Location = ""))
+
+    x_summarised <- aggregate(Value ~ Site + Plot + TIMESTAMP + Instrument +
+                                  Instrument_ID + Sensor_ID + Location,
+                              data = x,
+                              FUN = mean, na.action = na.omit, drop = FALSE)
+    # Summarise number of used and not used values
+    x$F_keep <- !x$F_drop
+    x2 <- aggregate(F_keep ~ Site + Plot + TIMESTAMP + Instrument +
+                        Instrument_ID + Sensor_ID + Location,
+                    data = x,
+                    FUN = sum, na.action = na.omit, drop = FALSE)
+
+    x3 <- aggregate(F_drop ~ Site + Plot + TIMESTAMP + Instrument +
+                        Instrument_ID + Sensor_ID + Location,
+                    data = x,
+                    FUN = sum, na.action = na.omit, drop = FALSE)
+    x_summarised$n <- x2$F_keep
+    x_summarised$n_drop <- x3$F_drop
+    return(x_summarised)
+}
+
 
 # Complete the L2 data: smooth time series for all combinations of metadata
 # We do each plot separately because they may have been
