330a57409f193bbac169e29b8d5be71084b3b0d5
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-06-18 08:18:47 -0400

    Move site-metadata insertion code to helper file

--- pipeline/metadata-utils.R
+++ pipeline/metadata-utils.R
@@ -1,5 +1,5 @@
 # metadata-utils.R
-# Helper functions for generating L1 and L2 metadata
+# Helper functions for generating both L1 and L2 metadata
 # BBL June 2025
 
 # Get information about files in a folder and insert into metadata
@@ -22,4 +22,28 @@ md_insert_fileinfo <- function(folder, md) {
     file_info_pos <- grep("[FILE_INFO", md, fixed = TRUE)
     md <- append(md, head(file_info, -1), after = file_info_pos)
     md[-file_info_pos]
+}
+
+# Read the variable metadata table and return a formatted extract from it
+md_variable_info <- function(variable_md_file) {
+    var_md <- read.csv(variable_md_file)
+    paste(sprintf("%-20s", c("research_name", var_md$research_name)),
+          sprintf("%-10s", c("Units", var_md$final_units)),
+          sprintf("%-12s", c("Bounds", paste0(var_md$low_bound, ", ", var_md$high_bound))),
+          c("Description", var_md$description))
+}
+
+# Insert site information into metadata
+# There MUST be an informational file named <site>.txt
+md_insert_siteinfo <- function(site, site_files_folder, md) {
+    site_md_file <- file.path(site_files_folder, paste0(site, ".txt"))
+    if(!file.exists(site_md_file)) {
+        stop("Couldn't find file ", site_md_file, " in ", site_files_folder)
+    }
+    site_md_for_insert <- readLines(site_md_file)
+
+    # Insert site information
+    site_info_pos <- grep("[SITE_INFO]", md, fixed = TRUE)
+    md <- append(md, site_md_for_insert, after = site_info_pos)
+    md[-site_info_pos]
 }
