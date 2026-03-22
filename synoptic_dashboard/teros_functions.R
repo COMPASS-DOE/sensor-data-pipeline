a8ed3dd2e3bfc0ba4422049e0bb5e0d2552af8ad
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2026-03-22 13:06:41 -0400

    Rework TEROS file processing code

--- synoptic_dashboard/teros_functions.R
+++ synoptic_dashboard/teros_functions.R
@@ -1,44 +1,36 @@
 
 
 fileread <- function(data, total_files) {
-
+  
   # If we're running in a Shiny session, update progress bar
   if(!is.null(getDefaultReactiveDomain())) {
     incProgress(1 / total_files)
   }
-
+  
   # Download files to local (don't worry, we'll delete em later)
-  drive_download(data, overwrite = T, path = "tempfile.dat")
-
+  drive_download(data, overwrite = TRUE, path = "tempfile.dat")
+  
   # Lines 1, 3, and 4 of the TEROS data files contain sensor metadata that we want to remove
   # Read the data files into a string vector, remove those lines, and then pass to read.csv()
   teros_primitive <- read_lines("tempfile.dat") #temp file goes here
-  teros_primitive <- teros_primitive[-3:-4]
-
+  teros_primitive <- teros_primitive[c(-1, -3, -4)]
+  
   unlink("tempfile.dat")
-
-  #textConnection(teros_primitive) %>%
-    read_csv(I(teros_primitive),
-             skip = 1,
-             #check.names = FALSE,
-             na = "NAN",
-             #na.strings = "NAN",
-             #stringsAsFactors = FALSE,
-             col_types = "Tddcddddddddddddddddddd") %>%
-    as_tibble() %>%
+  
+  read_csv(I(teros_primitive),
+           na = "NAN",
+           col_types = "cddcddddddddddddddddddd") %>%
     # Reshape the data frame to one observation per row
-    gather(channel, value, -TIMESTAMP, -RECORD, -Statname) %>%
-    filter(!is.na(value)) %>%
-    # Pull data logger ID out of statname
-    separate(Statname, into = c("Inst", "Logger"), sep = "_" ) %>%
-    # Parse the data logger number, channel number, and variable number out of the
-    # Statname and Channel columns
-    mutate(Logger = as.integer(Logger, fixed = TRUE),
-           TIMESTAMP = ymd_hms(TIMESTAMP, tz = "EST")) %>%
-    select(-Inst) %>%  # unneeded
+    gather(channel, value, -TIMESTAMP, -RECORD, -PB, -Statname) %>%
+    filter(!is.na(value), grepl("Teros", channel, ignore.case = TRUE)) %>%
+    # PB is the logger number and Statname holds site and plot info
+    rename(Logger = PB, Site = Statname) %>% 
+    mutate(Logger = as.factor(Logger),
+           TIMESTAMP = ymd_hms(TIMESTAMP, tz = "EST"),
+           Site = gsub("COMPASS_", "", Site, ignore.case = TRUE)) %>%
     # Next, parse channel into the data logger channel and variable number
     separate(channel, into = c("Data_Table_ID", "variable"), sep = ",") %>%
-    mutate(Data_Table_ID = as.integer(gsub("Teros(", "", Data_Table_ID, fixed = TRUE)),
+    mutate(Data_Table_ID = as.integer(gsub("Teros[A-Z]?\\(", "", Data_Table_ID)),
            variable = as.integer(gsub(")", "", variable, fixed = TRUE)),
            # Give them sensible names
            variable = case_when(variable == 1 ~ "VWC",
@@ -48,18 +40,18 @@ fileread <- function(data, total_files) {
 
 
 process_teros <- function() {
-
+  
   # Create a list of files
   cat("Accessing drive..")
   teros_files <- gdrive_files %>%
     filter(grepl("TerosTable", name))
-
+  
   #NEED TO ADD INVENTORY LATER
-
-teros_files$name %>%
+  
+  teros_files$name %>%
     map(fileread, nrow(teros_files)) %>%
     bind_rows()
-
+  
   # teros_primitive %>%
   #   left_join(teros_inventory, by = c("Logger" = "Data Logger ID",
   #                                     "Data_Table_ID" = "Terosdata table channel")) %>%
@@ -76,5 +68,5 @@ teros_files$name %>%
   #     distinct(Logger, Data_Table_ID) %>%
   #     kable()
   # }
-
+  
 }
